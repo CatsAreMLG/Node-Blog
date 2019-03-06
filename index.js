fc837a4b67cf91640e0eba0006cc45d895b51439
CatsAreMLG <joshuadgon@gmail.com>
2019-03-05 20:53:23 -0600

    Add all functions for users

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const PostsRouter = require('./data/routers/postsRouter.js')
+const UsersRouter = require('./data/routers/usersRouter.js')
 const server = express()
 server.use(express.json())
 server.use('/api/posts', PostsRouter)
+server.use('/api/users', UsersRouter)
 const PORT = 8888
 
 server.get('/', (req, res) => {
