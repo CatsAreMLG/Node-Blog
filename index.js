86c9c1c8cbd3914137d31b4c2a0807740a51f1b7
CatsAreMLG <joshuadgon@gmail.com>
2019-03-05 20:05:32 -0600

    Add base server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code away!
+const express = require('express')
+const server = express()
+server.use(express.json())
+const PORT = 8888
+
+server.get('/', (req, res) => {
+  res.send(`<h2>Welcome to the API</h2>`)
+})
+
+server.listen(PORT, _ => {
+  console.log(`SERVER RUNNING ON http://localhost:${PORT}`)
+})
