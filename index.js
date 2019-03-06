2adb51f024a058b87be666bcec5bf34857faa21d
CatsAreMLG <joshuadgon@gmail.com>
2019-03-05 21:29:24 -0600

    Add capital letters middleware

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const PostsRouter = require('./data/routers/postsRouter.js')
 const UsersRouter = require('./data/routers/usersRouter.js')
 const server = express()
 server.use(express.json())
+server.use(userCaps)
 server.use('/api/posts', PostsRouter)
 server.use('/api/users', UsersRouter)
+function userCaps(req, res, next) {
+  if (req.body.name)
+    req.body.name = req.body.name.split` `.map(
+      n => n.charAt(0).toUpperCase() + n.substring(1)
+    ).join` `
+  next()
+}
 const PORT = 8888
 
 server.get('/', (req, res) => {
